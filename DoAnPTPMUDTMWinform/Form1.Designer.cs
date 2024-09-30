9935ae0170a70e602546fe456d1d1137aace8d50
tinhbe <98801099+tvantinh@users.noreply.github.com>
2024-09-30 22:33:29 +0700

    update db

--- DoAnPTPMUDTMWinform/Form1.Designer.cs
+++ DoAnPTPMUDTMWinform/Form1.Designer.cs
@@ -29,29 +29,20 @@
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
-            userControl11 = new ControlsLibary.UserControl1();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(125, 102);
+            textBox1.Location = new Point(112, 123);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 0;
-            // 
-            // userControl11
-            // 
-            userControl11.Location = new Point(286, 205);
-            userControl11.Name = "userControl11";
-            userControl11.Size = new Size(243, 216);
-            userControl11.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(userControl11);
             Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
